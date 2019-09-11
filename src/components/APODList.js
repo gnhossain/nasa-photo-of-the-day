07e4d0816906ac200e41dc9aca766b735005fba4
G H <gh@Gs-MacBook-Pro.local>
2019-09-11 15:22:55 -0700

    stretch

--- src/components/APODList.js
+++ src/components/APODList.js
@@ -5,6 +5,7 @@ import axios from "axios";
 export default function APODList() {
   // NOTE: The value given to useState() must be of the same type as your value is expected to be
     const [photo, setPhoto] = useState([]);
+    const [photo2, setPhoto2] = useState([]);
 
     useEffect(() => {
     axios
@@ -18,11 +19,25 @@ export default function APODList() {
         console.log("The data was not returned", error);
       });
     }, []);
+    useEffect(() => {
+        axios
+          .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+          .then(response => {
+            const photo2 = response.data;
+            console.log("NASA APOD", photo2);
+            setPhoto2(photo2);
+          })
+          .catch(error => {
+            console.log("The data was not returned", error);
+          });
+        }, []);
+        
     
     return (
         <div className="APOD">
         
             <APODCard apod={photo}/>
+            <APODCard apod={photo2}/>
         </div>
     );
         
