6e69526417145b04eee10c4c91a3b6f9456cdd57
G H <gh@Gs-MacBook-Pro.local>
2019-09-12 14:46:30 -0700

    mvp part ll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 import APODList from "./components/APODList";
 import "./App.css";
 
+const Title = styled.h1`
+  font-size: 3.5em;
+  text-align: center;
+  color: DeepSkyBlue;
+
+  padding: .5em;
+  background: Lavender;
+`;
+
+
 function App() {
   return (
     <div className="App">
-      <h1>NASA APOD</h1>
+      <Title>NASA APOD</Title>
+      {/* <h1>NASA APOD</h1> */}
       <p>
         {/* Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀! */}
--- src/components/APODCard.js
+++ src/components/APODCard.js
@@ -1,15 +1,33 @@
 import React from "react";
 
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
 const APODCard = (props) => {
 
     console.log(props);
   return (
-    <div className="nasa-photo">
-      <h2>Title: {props.apod.title}</h2>
-      <p>Date: {props.apod.date}</p>
-      <img src={props.apod.hdurl}/>
-      <p>Explanation: {props.apod.explanation}</p>
+    // <div className="nasa-photo">
+    //   <h2>Title: {props.apod.title}</h2>
+    //   <p>Date: {props.apod.date}</p>
+    //   <img src={props.apod.hdurl}/>
+    //   <p>Explanation: {props.apod.explanation}</p>
+    // </div>
+
+    <div className='APOD-card'>
+      <Card>
+        <CardImg top width="100%" src={props.apod.hdurl} alt="Nasa APOD" />
+        <CardBody>
+          <CardTitle>Title: {props.apod.title}</CardTitle>
+          <CardSubtitle>Date: {props.apod.date}</CardSubtitle>
+          <CardText>Explanation: {props.apod.explanation}</CardText>
+          {/* <Button>Button</Button> */}
+        </CardBody>
+      </Card>
     </div>
+
   );
 };
 
